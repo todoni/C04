acee16d26582b7467f93e200e2efe731b416d4ec
root <root@.localdomain>
2021-04-04 23:54:24 +0900

    some {} added in ex03

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sohan <sohan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 21:08:59 by sohan             #+#    #+#             */
-/*   Updated: 2021/04/04 03:12:02 by sohan            ###   ########.fr       */
+/*   Updated: 2021/04/04 23:36:38 by sohan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,36 @@ int	ft_atoi(char *str)
 	multiple = 1;
 	i = 0;
 	check = 0;
+	return_value = 0;
 	while (*str != '\0')
 	{
 		if (*str == '-')
 		{
 			minus++;
 		}
-		while (*str >= '0' && *str <= '9' && *str != '\0'){
+		while (*str >= '0' && *str <= '9' && *str != '\0')
+		{
 			integer[i] = *str - 48;
 			if (multiple <= 100000000)
+			{
 				multiple = multiple * 10;
+			}
 			else
+			{
 				multiple = multiple * 1;
+			}	
 			str++;
 			i++;
-			check = 1;
+			check = 1;	
 		}
 		if (i != 10 && check != 0)
 		{	
 			multiple = multiple / 10;
 		}
 		if (minus % 2 != 0 && check != 0)
+		{	
 			multiple = -multiple;
+		}
 		i = 0;
 		while(multiple != 1 && multiple != -1)
 		{	return_value = return_value + integer[i] * multiple; 
@@ -65,6 +73,6 @@ int	ft_atoi(char *str)
 
 int main()
 {
-	printf("%d",ft_atoi("  --+---+0000asdf345"));
+	printf("%d",ft_atoi("  --+--+2147483648asdf345"));
 	return 0;
 }
